708405f1b63af83966dd0e9733e916ce0f5437fa
Justin <sumbhodi1618@gmail.com>
2019-06-03 01:08:38 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -14,7 +14,11 @@
 
 def help
   #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands: "
+  puts "-help : displays this help message"
+  puts "-list : displays a list of songs you can displays"
+  puts "-play : lets you choose a song to -play"
+  puts "-exit : exits this program"
 end
 
 
